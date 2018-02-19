6a43134beed3bfa652eb7206b6df1241a63c9ff9
Teodor Bogoeski <teodorbogoeski@gmailcom>
2018-02-19 20:37:58 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class App extends React.Component {
+    render(){
+        return (
+        <div>
+                <h1 className='text-center heading'>Game Of Life</h1>
+        </div>
+        );
+    }
+}
